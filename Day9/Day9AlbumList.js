06f63f26c2cca9a1a9a57d06bc9967897dfd1ad9
anisha-g-3 <727723eucs016@skcet.ac.in>
2024-10-26 12:12:39 +0530

    Add files via upload

---
+++ Day9/Day9AlbumList.js
@@ -0,0 +1,25 @@
+//AlbumList
+import React from 'react';
+function Albumslist()
+{
+    const album = [
+        { name: 'Thriller', singer: 'Michael Jackson' },
+        { name: 'Like a Virgin', singer: 'Madonna' },
+        { name: 'A Night at the Opera', singer: 'Queen' },
+        { name: 'Blue Hawaii', singer: 'Elvis Presley' },
+      ];
+    
+return(
+    <div>
+<h2>Albums</h2>
+<ul>
+    {album.map((a,index)=>(
+        <li key={index}>
+            {a.name} - by {a.singer}
+        </li>
+    ))}
+</ul>
+    </div>
+)
+}
+export default Albumslist;
